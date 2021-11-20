0d7d82020793ee87a6f04c82f910da53b62892cf
Ruslanec777 <moi_mail_www@mail.ru>
2021-11-20 19:48:28 +0300

    Добавьте файлы проекта.

---
+++ Steam_v2/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Steam_v2
+{
+    public class Class1
+    {
+    }
+}
---
+++ Steam_v2/Enums/Sex.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Steam_v2.Enums
+{
+    public enum Sex
+    {
+        Man,
+        Woman
+    }
+
+}
